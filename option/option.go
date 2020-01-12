abca5e151a2a36fba9685cfafe69f2d9d5fb8567
maitaken <150441004@ccmailg.meijo-u.ac.jp>
2020-01-12 16:48:52 +0900

    Fixed : ファイルの指定にワイルドカードを使用できるように変更

--- option/option.go
+++ option/option.go
@@ -1,6 +1,9 @@
 package option
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/urfave/cli"
 )
 
@@ -23,18 +26,27 @@ func GetOption() *Option {
 
 func SetOption(c *cli.Context) {
 	// 引数とオプションの整理
-	if files := c.StringSlice("f"); len(files) != 0 {
-		opt.TargetFile = make([]string, len(files))
-		copy(opt.TargetFile, files)
+	if paths := c.StringSlice("f"); len(paths) != 0 {
+
+		for _, path := range paths {
+			p, e := filepath.Glob(path)
+			if e != nil {
+				os.Exit(1)
+			}
+			opt.TargetFile = append(opt.TargetFile, p...)
+		}
 
 	} else {
-		opt.TargetFile = make([]string, c.NArg()-1)
 		for i := 0; i < c.NArg()-1; i++ {
-			opt.TargetFile[i] = c.Args().Get(i)
+			path := c.Args().Get(i)
+
+			p, e := filepath.Glob(path)
+			if e != nil {
+				os.Exit(1)
+			}
+			opt.TargetFile = append(opt.TargetFile, p...)
 		}
-		opt.TargetFile[0] = c.Args().Get(0)
 	}
 
 	opt.Cmd = c.Args().Get(c.NArg() - 1)
-
 }
